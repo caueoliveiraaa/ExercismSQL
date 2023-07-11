59f2cff146833ef3fa68b8a1d02fe251c96d841e
caueoliveiraaa <leo.siscobra@gmail.com>
2023-07-10 21:20:42 -0300

    update

--- 2_banco_de_dados/aula_1_and_2/0_create_insert.sql
+++ 2_banco_de_dados/aula_1_and_2/0_create_insert.sql
@@ -1,3 +1,71 @@
+-- database: c:\Users\Leôncio Cauê\Desktop\apex_python_junho_2023\2_banco_de_dados\aula_1_and_2\mydatabase.db
+
+-- Use the ▷ button in the top right corner to run the entire file.
+
+-- 1 - Criar uma tabela
+-- PRIMARY KEY:
+-- Auto incrementa o valor que está no campo
+-- Força o campo a não ter valor nulo
+-- Garantir que cada usuário tenha um valor único
+
+-- IF NOT EXISTS:
+-- Garante que a tabela apenas será criada
+-- caso uma tabela com o nome informado não exista!
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    -- Campo do tipo inteiro, com chave primária
+    id INTEGER PRIMARY KEY,
+    -- TEXT é usado para campos do tipo string
+    nome TEXT(100),
+    -- INTEGER é usado para campos com números inteiros
+    idade INTEGER,
+    -- O último campo não leva vírgula
+    email TEXT(100)
+);
+
+-- 2 - Inserindo dados na tabela criada:
+-- INSERT INTO é o comando SQL para inserir dados nas tabelas
+INSERT INTO usuarios (nome, idade, email)
+-- É seguido pela palavra VALUES que armazena os valores nas colunas
+VALUES ('Bryan', 25, 'bryan@gmail.com');
+
+-- Inserindo mais dados na tabela:
+INSERT INTO usuarios (nome, idade, email)
+VALUES ('Cauê', 26, 'caue@gmail.com');
+
+INSERT INTO usuarios (nome, idade, email)
+VALUES ('Bruno', 28, 'bruno@gmail.com');
+
+INSERT INTO usuarios (nome, idade, email)
+VALUES ('Amanda', 14, 'amanda@gmail.com');
+
+INSERT INTO usuarios (nome, idade, email)
+VALUES ('Jim', 5, 'jim@gmail.com');
 
 
--- 1 - Criando tabelas:
+-- 3 - Alterando tebelas:
+-- ALTER TABLE é usadado para alterar tabelas
+-- RENAME TO que renomeia a tabela
+
+ALTER TABLE usuarioss
+RENAME TO usuarios;
+
+-- ADD COLUMN adiciona uma coluna
+ALTER TABLE usuarios
+ADD COLUMN coluna_nova;
+
+-- DROP COLUMN 
+ALTER TABLE usuarios
+DROP COLUMN coluna_nova;
+
+
+-- 4 Resetando tabelas
+-- DELETE FROM deleta todos os dados
+-- da tabela se não for passado uma
+-- condição específica.
+-- VACUUM - zerar o primary key
+
+DELETE FROM usuarios;
+
+-- 5 Destruindo tabelas:
+DROP TABLE usuarios;
---
+++ 2_banco_de_dados/aula_1_and_2/1_create_insert.py
@@ -0,0 +1,88 @@
+# No começo do arquivo realizamos as importações necessárias
+# Biblioteca sqlite que serve para manipular bancos de dados do sqlite
+import sqlite3
+# Biblioteca os que server para manipular o sistema operacional
+import os
+
+os.system('cls')
+
+# Conexão com o banco de dados
+conexao = sqlite3.connect('mydatabase.db')
+print('Conexão criada com sucesso.')
+
+# Cursor para manipular o banco de dados
+cursor = conexao.cursor()
+
+# Variável para criar a nova tabela
+nome_tabela = ''
+
+# OBS: o True e False (valores booloan),
+# começam com letra maiúscula em Python
+while True:
+    # Lero o nome da tabela informado pelo usuário
+    nome_tabela = input('Informe o nome da tabela: ')
+
+    # Verificar se o nome é válido
+    if nome_tabela != '' and len(nome_tabela) > 3:
+        print(f'Nome {nome_tabela} é válido para tabela!')
+        break
+    else:
+        print('Informe um nome válido!')
+
+# Executar comandos SQL no banco de dados
+# com a função execute da biblioteca sqlite
+cursor.execute(f'''
+
+    CREATE TABLE IF NOT EXISTS {nome_tabela} (
+        id INTEGER PRIMARY KEY,
+        nome TEXT,
+        idade INTEGER,
+        email TEXT
+    )
+
+''')
+
+print('Tabela criada com sucesso!\n')
+
+# Pedir os dados ao usuário
+# para preencher a tabela
+usuario_nome = ''
+usuario_idade = 0
+usuario_email = ''
+
+while True:
+    # Ler dados
+    # OBS: input por padrão recebe strings/textos
+    usuario_nome = input('Informe o nome do usuário: ')
+    usuario_idade = input('Informe a idade do usuário: ')
+    usuario_email = input('Informe o e-mail do usuário: ')
+
+    # Converter idade para inteiro
+    usuario_idade = int(usuario_idade)
+
+    # Validar os dados armazenando valores True ou False
+    # em variáveis de validação
+
+    validacao_1 = usuario_nome != '' and len(usuario_nome) >= 3
+    validacao_2 = usuario_idade > 10 and usuario_idade < 100
+    validacao_3 = usuario_email != '' and '@' in usuario_email
+
+    if validacao_1 and validacao_2 and validacao_3:
+        print(f'Nome {usuario_nome}, idade {usuario_idade}, e-mail {usuario_email} são dados válidos!')
+        break
+    else:
+        print(f'Nome é válido: {validacao_1}')
+        print(f'Idade é válida: {validacao_2}')
+        print(f'E-mail é válido: {validacao_3}')
+        print('Informes dados válidos!')
+
+
+# Inserir os dados na nossa base de dados
+cursor.execute(f'''
+
+    INSERT INTO {nome_tabela} (nome, idade, email)
+    VALUES ('{usuario_nome}', {usuario_idade}, '{usuario_email}')
+
+''')
+
+print('Dados inseridos com sucesso!')
