281d8b713e65d773b025c11645bac18ec2570b35
caueoliveiraaa <leo.siscobra@gmail.com>
2023-08-07 18:20:38 -0300

    update

---
+++ 3_python_fundamentos/aula_1_and_2/exercicios/exercicio_09.py
@@ -0,0 +1,6 @@
+# Exercício 9:
+# Crie uma função que converta um valor em dólar para real.
+#  Peça ao usuário que insira a cotação do dólar e o valor em dólar
+#  a ser convertido. Em seguida, exiba o valor equivalente em reais.
+
+
