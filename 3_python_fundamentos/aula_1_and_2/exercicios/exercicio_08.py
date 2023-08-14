dee578736cac516d2e261a3467e0fbe1071f32e4
caueoliveiraaa <leo.siscobra@gmail.com>
2023-08-14 18:36:31 -0300

    update

--- 3_python_fundamentos/aula_1_and_2/exercicios/exercicio_08.py
+++ 3_python_fundamentos/aula_1_and_2/exercicios/exercicio_08.py
@@ -10,10 +10,13 @@ def recebe_data():
     while True:
         try:
             dia = int(input('Informe o dia: '))
+
             if dia > 0 and dia <= 31:
                 mes = int(input('Informe o mes: '))
+
                 if mes > 0 and mes <= 12:
                     ano = int(input('Informe o ano: '))
+                    
                     if ano >= 1950 and ano <= 2023:
                         return dia, mes, ano
                     else:
