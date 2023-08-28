4af0eb9e0b9e7a7880e51580701401e721dc283e
caueoliveiraaa <leo.siscobra@gmail.com>
2023-08-28 19:47:12 -0300

    update

--- 3_python_fundamentos/aula_7_and_8/review.py
+++ 3_python_fundamentos/aula_7_and_8/review.py
@@ -21,18 +21,34 @@ def sao_numeros_iguais(numero_parametro_1, numero_parametro_2):
 
 # funcao para verificar e retornar qual numero de dois numeros eh o maior
 # se ambos forem numeros positivos
-def funcao():
-    ...
+def retorna_maior_numero(numero_parametro_1, numero_parametro_2):
+    numero_maior = None
 
+    if eh_positivo(numero_parametro_1) and eh_positivo(numero_parametro_2):
+        if numero_parametro_1 > numero_parametro_2:
+            numero_maior = numero_parametro_1
+        elif numero_parametro_1 < numero_parametro_2:
+            numero_maior = numero_parametro_2
+        else:
+            print('Os números são iguais! Informe números diferentes!')
+    else:
+        print('Informar apenas números positivos!')
+
+    return numero_maior
 
 
 # funcao para verificar qual numero de dois numeros eh menor
 # se ambos forem numeros positivos
-def funcao():
+def retorna_menor_numero(numero_parametro_1, numero_parametro_2):
+
     ...
 
 
-
+# funcao para verificar se um numero eh impar ou par
+# se ambos forem numeros positivos
+def verifica_se_eh_impar_ou_par(numero_parametro):
+    
+    ...
 
 
 
@@ -47,8 +63,8 @@ def main():
     0 - Sair
     1 - eh_positivo
     2 - sao_numeros_iguais
-    3 - 
-    4 -
+    3 - retorna_maior_numero
+    4 - retorna_menor_numero
     5 -
     6 -
     --> opcao: '''
@@ -64,7 +80,7 @@ def main():
             elif funcao_selecionada == 1:
                 numero = int(input('Informe um número: '))
 
-                if eh_positivo(numero) is True:
+                if eh_positivo(numero) == True:
                     print(f'O número {numero} é positivo')
                 else:
                     print(f'O número {numero} é negativo')
@@ -73,14 +89,19 @@ def main():
                 numero_1 = int(input('Informe o primeiro número: '))
                 numero_2 = int(input('Informe o segundo número: '))
 
-                if sao_numeros_iguais(numero_1, numero_2) is True:
+                if sao_numeros_iguais(numero_1, numero_2) == True:
                     print(f'O número {numero_1} é igual ao número {numero_2}')
                 else:
                     print(f'O número {numero_1} é diferente do número {numero_2}')
 
             elif funcao_selecionada == 3:
+                numero_1 = int(input('Informe o primeiro número: '))
+                numero_2 = int(input('Informe o segundo número: '))
+                
+                numero_retornado = retorna_maior_numero(numero_1, numero_2)
+                if numero_retornado != None:
+                    print(f'O maior número é: {numero_retornado}')
 
-                ...
 
             elif funcao_selecionada == 4:
 
