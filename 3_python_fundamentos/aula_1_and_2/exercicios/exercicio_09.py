78935b17d60ebffc063de794cd1a9b9a3d13f00c
caueoliveiraaa <leo.siscobra@gmail.com>
2024-06-10 22:01:56 -0300

    Reset
    Reset

--- 3_python_fundamentos/aula_1_and_2/exercicios/exercicio_09.py
+++
@@ -1,9 +0,0 @@
-# Exercício 9:
-# Crie uma função que converta um valor em dólar para real.
-#  Peça ao usuário que insira a cotação do dólar e o valor em dólar
-#  a ser convertido. Em seguida, exiba o valor equivalente em reais.
-
-
-
-
-
--- 3_python_fundamentos/aula_5_and_6/exercicios/exercicio_004.py
+++
@@ -1,5 +0,0 @@
-# 4 - Crie uma classe abstrata Animal com um método emitir_som(). Crie subclasses Cachorro,
-# Gato, Cavalo e Vaca que herdam de Animal e implementam seus próprios sons. Crie uma função
-# que aceita uma lista de animais e faça-os emitir sons usando polimorfismo.
-# Em seguida, crie contrutores para as subclasses dando um atributo nome e cor, e crie
-# objetos dessas subclasses com cores e nomes distintos.
